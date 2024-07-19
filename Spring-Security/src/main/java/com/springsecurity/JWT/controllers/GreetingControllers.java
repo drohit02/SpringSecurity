2f4fb964cc5b0dd2cd5998680bccfe4fded7e7f0
drohit02 <drohit045@gmail.com>
2024-07-19 18:00:56 +0530

    Testing API with the PreAuthorize and who can access it specified

--- Spring-Security/src/main/java/com/springsecurity/JWT/controllers/GreetingControllers.java
+++ Spring-Security/src/main/java/com/springsecurity/JWT/controllers/GreetingControllers.java
@@ -1,5 +1,6 @@
 package com.springsecurity.JWT.controllers;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +11,17 @@ public class GreetingControllers {
 	public String sayHello() {
 		return "Hello,Spring-Security";
 	}
+	
+	@PreAuthorize("hasRole('USER')")
+	@GetMapping("/user")
+	public String sayHelloToUser() {
+		return "Hello,User";
+	}
+	
+	@PreAuthorize("hasRole('ADMIN')")
+	@GetMapping("/admin")
+	public String sayHelloToAdminr() {
+		return "Hello,Admin";
+	}
+	
 }
