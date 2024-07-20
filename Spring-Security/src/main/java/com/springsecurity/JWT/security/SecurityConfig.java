1d22040ea7793bff7ad03a52447c7941c9c919d9
drohit02 <drohit045@gmail.com>
2024-07-20 20:00:42 +0530

    JdbcUserDetailManager implemented

--- Spring-Security/src/main/java/com/springsecurity/JWT/security/SecurityConfig.java
+++ Spring-Security/src/main/java/com/springsecurity/JWT/security/SecurityConfig.java
@@ -10,15 +10,24 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
 
+	@Autowired
+	private DataSource dataSource;
+	
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
     	  /* authenticating every request */
@@ -36,6 +45,21 @@ public class SecurityConfig {
     }
     
     
+//    @Bean
+//    UserDetailsService userDetailsService() {
+//    	UserDetails user1 = User.withUsername("user1")
+//    							.password("{noop}pass123")
+//    							.roles("USER")
+//    							.build();
+//    	
+//    	UserDetails admin = User.withUsername("admin")
+//    							.password("{noop}admin123")
+//    							.roles("ADMIN")
+//    							.build();
+//    	
+//    	return new InMemoryUserDetailsManager(user1,admin);
+//    }
+//    
     @Bean
     UserDetailsService userDetailsService() {
     	UserDetails user1 = User.withUsername("user1")
@@ -43,12 +67,16 @@ public class SecurityConfig {
     							.roles("USER")
     							.build();
     	
-    	UserDetails admin = User.withUsername("admin")
-    							.password("{noop}admin123")
-    							.roles("ADMIN")
-    							.build();
+    	UserDetails admin1 = User.withUsername("admin1")
+				.password("{noop}admin123")
+				.roles("ADMIN")
+				.build();
+
+    	JdbcUserDetailsManager jdbcUserDetailsManager  = new JdbcUserDetailsManager(dataSource);
+    	jdbcUserDetailsManager.createUser(user1);
+    	jdbcUserDetailsManager.createUser(admin1);
+    	return jdbcUserDetailsManager;
     	
-    	return new InMemoryUserDetailsManager(user1,admin);
     }
 
 }
