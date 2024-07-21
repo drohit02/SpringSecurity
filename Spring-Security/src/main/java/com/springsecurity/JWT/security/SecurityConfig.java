4e92bb214e405324217b219cb01f41fd42ccd5b3
drohit02 <drohit045@gmail.com>
2024-07-21 18:10:41 +0530

    SecurityConfig modified added the our custom authfilter in filter chain

--- Spring-Security/src/main/java/com/springsecurity/JWT/security/SecurityConfig.java
+++ Spring-Security/src/main/java/com/springsecurity/JWT/security/SecurityConfig.java
@@ -1,7 +1,12 @@
 package com.springsecurity.JWT.security;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,15 +17,11 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
-import javax.sql.DataSource;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.AutoConfigureOrder;
+import com.springsecurity.JWT.jwt.AuthEntryPointJwt;
+import com.springsecurity.JWT.jwt.AuthTokenFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -29,39 +30,45 @@ public class SecurityConfig {
 
 	@Autowired
 	private DataSource dataSource;
-	
-    @Bean
-    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-    	  /* authenticating every request */
-         http.authorizeHttpRequests((request)->request.anyRequest().authenticated());
-         
-         /* Making every request stateless */
-         http.sessionManagement((session)->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-         
-         /* enable the Form-Based Authentication functionality using HTML Form for Login and Logout*/
-         //http.formLogin(withDefaults());
-         
-         /* enable the Basic Authentication functionality using Browser Pop-Up*/
-         http.httpBasic(withDefaults());
-         return http.build();
-    }
-    
-    
-    @Bean
-    UserDetailsService userDetailsService() {
-    	UserDetails user1 = User.withUsername("user1")
-    							.password(passwordEncoder().encode("pass123"))
-    							.roles("USER")
-    							.build();
-    	
-    	UserDetails admin = User.withUsername("admin")
-    							.password(passwordEncoder().encode("admin123"))
-    							.roles("ADMIN")
-    							.build();
-    	
-    	return new InMemoryUserDetailsManager(user1,admin);
-    }
-    
+
+	@Autowired
+	private AuthEntryPointJwt unauthorizedHandler;
+
+	@Bean
+	public AuthTokenFilter authenticationJwtTokenFilter() {
+		return new AuthTokenFilter();
+	}
+
+	 @Bean
+	    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+	        http.authorizeHttpRequests(authorizeRequests ->authorizeRequests.requestMatchers("/signin").permitAll()
+	                        .anyRequest().authenticated());
+	        http.sessionManagement(session ->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+	        http.exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler));
+	        //http.httpBasic(withDefaults());
+	        http.headers(headers -> headers.frameOptions(frameOptions -> frameOptions.sameOrigin()));
+	        http.csrf(csrf -> csrf.disable());
+	        http.addFilterBefore(authenticationJwtTokenFilter(),UsernamePasswordAuthenticationFilter.class);
+
+	        return http.build();
+	    }
+
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration builder) throws Exception {
+		return builder.getAuthenticationManager();
+	}
+
+	@Bean
+	UserDetailsService userDetailsService() {
+		UserDetails user1 = User.withUsername("user1").password(passwordEncoder().encode("pass123")).roles("USER")
+				.build();
+
+		UserDetails admin = User.withUsername("admin").password(passwordEncoder().encode("admin123")).roles("ADMIN")
+				.build();
+
+		return new InMemoryUserDetailsManager(user1, admin);
+	}
+
 //    @Bean
 //    UserDetailsService userDetailsService() {
 //    	UserDetails user1 = User.withUsername("user1")
@@ -79,10 +86,10 @@ public class SecurityConfig {
 //    	jdbcUserDetailsManager.createUser(admin1);
 //    	return jdbcUserDetailsManager;
 //    }
-    
-    @Bean
-    PasswordEncoder passwordEncoder() {
-    	return new BCryptPasswordEncoder(12);
-    }
+
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder(12);
+	}
 
 }
